df99b9e9614bee6ca05a40e18d84d98afceb98c0
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-16 23:28:17 -0500

    date complete

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -21,18 +21,33 @@ const addDateSuffix = (date) => {
     ) => {
         const months ={
             0: monthLength === 'short' ? 'jan' : 'january',
-            1: monthLength === 'short' ? 'feb' : 'january',
-            2: monthLength === 'short' ? 'mar' : 'january',
-            3: monthLength === 'short' ? 'apr' : 'january',
-            4: monthLength === 'short' ? 'may' : 'january',
-            5: monthLength === 'short' ? 'jun' : 'january',
-            6: monthLength === 'short' ? 'jul' : 'january',
-            7: monthLength === 'short' ? 'aug' : 'january',
-            8: monthLength === 'short' ? 'sep' : 'january',
-            9: monthLength === 'short' ? 'oct' : 'january',
-            10: monthLength === 'short' ? 'nov' : 'january',
-            11: monthLength === 'short' ? 'dec' : 'january',
-            
+            1: monthLength === 'short' ? 'feb' : 'february',
+            2: monthLength === 'short' ? 'mar' : 'march',
+            3: monthLength === 'short' ? 'apr' : 'april',
+            4: monthLength === 'short' ? 'may' : 'may',
+            5: monthLength === 'short' ? 'jun' : 'june',
+            6: monthLength === 'short' ? 'jul' : 'july',
+            7: monthLength === 'short' ? 'aug' : 'august',
+            8: monthLength === 'short' ? 'sep' : 'september',
+            9: monthLength === 'short' ? 'oct' : 'october',
+            10: monthLength === 'short' ? 'nov' : 'november',
+            11: monthLength === 'short' ? 'dec' : 'december',
+    };
+    const dateObj = new date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
 
+    const dayOfMonth = dateSuffix
+    ? addDateSuffix(dateObj.getDate())
+    : dateObj.getHours();
+
+    if (hour === 0){
+        hour = 12;
     }
-    }
+    const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+    
+    const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+    return formattedTimeStamp;
+    };
