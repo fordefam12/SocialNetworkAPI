d3203d3ffad553032108eb80ef8f1202e498e2e9
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-16 22:30:21 -0500

    routes complete

---
+++ routes/api/user-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addfriend,removeFriend,
+} =require("../../controllers/user-controller");
+
+router.route("/").get(getAllUser).post(createUser);
+
+router.route("/").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addfriend).delete(removeFriend);
+
+module.exports = router;
