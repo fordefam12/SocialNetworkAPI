1bbc19f4bdf6975fb684604fa50b5133b2685014
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-16 23:17:18 -0500

    date  modified

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -5,6 +5,34 @@ const addDateSuffix = (date) => {
 
     if(lastChar === '1' && datestr !=='11') {
         dateStr = `${dateStr}st`;
-        
+    }else if (lastChar === '2' && dateStr !== '12'){
+        dateStr = `${dateStr}nd`;
+    }else if (lastChar === '3' && dateStr !== '13'){
+        dateStr = `${dateStr}rd`;
+    } else {
+        dateStr = `${dateStr}th`;
     }
-}
+    return dateStr;
+    };
+
+    module.exports = (
+        timetamp,
+        {monthLength ='short', dateSuffix = true} = {}
+    ) => {
+        const months ={
+            0: monthLength === 'short' ? 'jan' : 'january',
+            1: monthLength === 'short' ? 'feb' : 'january',
+            2: monthLength === 'short' ? 'mar' : 'january',
+            3: monthLength === 'short' ? 'apr' : 'january',
+            4: monthLength === 'short' ? 'may' : 'january',
+            5: monthLength === 'short' ? 'jun' : 'january',
+            6: monthLength === 'short' ? 'jul' : 'january',
+            7: monthLength === 'short' ? 'aug' : 'january',
+            8: monthLength === 'short' ? 'sep' : 'january',
+            9: monthLength === 'short' ? 'oct' : 'january',
+            10: monthLength === 'short' ? 'nov' : 'january',
+            11: monthLength === 'short' ? 'dec' : 'january',
+            
+
+    }
+    }
